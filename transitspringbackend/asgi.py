57649112bf2f73ad8539523940b7ba9ca0cda1d1
Garen Sahagian <g.s.sahagian@gmail.com>
2024-06-17 11:19:39 -0400

    initial

---
+++ transitspringbackend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for transitspringbackend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transitspringbackend.settings')
+
+application = get_asgi_application()
