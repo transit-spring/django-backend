b89dd73fa931bb2c5c9e3bd28ede7999c5905181
Garen Sahagian <g.s.sahagian@gmail.com>
2024-06-18 13:19:09 -0400

    change out django for django-rest

--- transitspringapi/urls.py
+++ transitspringapi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for transitspringbackend project.
+URL configuration for transitspringapi project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
